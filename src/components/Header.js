81c90f178c86a40fc44d826cf8c43d96891f1633
Shedrack Sunday <Hannania002@gmail.com>
2022-11-14 18:48:34 +0100

    Add Header file with starter codes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Header = () => (
-  <header>
-    This is a Header
-  </header>
-);
+const Header = () => {
+  return (
+    <WrapperHeader>
+      <Container>
+        <div className='header-logo'>
+          <img src="" alt="logo"/>
+          <hi>Space Traverlers&apos; Hub</hi>
+        </div>
+        <nav>
+          <ul>
+            <li>Rockets</li>
+            <li>Mission</li>
+            <li>My Profile</li>
+          </ul>
+        </nav>
 
+      </Container>
+    </WrapperHeader>
+  );
+}
 export default Header;
