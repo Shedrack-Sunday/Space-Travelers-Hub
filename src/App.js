be71e7ac0164350a3302a562a6f1b285e4ddef2b
Shedrack Sunday <Hannania002@gmail.com>
2022-11-14 21:15:32 +0100

    Add sync lins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
+import { Header, Missions, Rockets, Profile } from ''
+import './App.css';
 
 function App() {
   return (
+    <Router className="App">
     <Header />
+      <Routes>
+        {/* <Route path="/" element{ <Rockets />} />
+        <Route path="/missions" element{ <Missions />} />
+        <Route path="/Profile" element{ <Profile />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
