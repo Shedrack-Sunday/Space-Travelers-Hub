5489bd2f37009f2cc10f93fe01f238b5f3c6fa44
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-11-15 18:32:18 +0330

    Change Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,18 @@
 import React from 'react';
-import styled from 'style-componenets';
 
-const WrapperHeader = styled.div`
-display: flex;
-flex-direction: row;
-align-item: center;
-justify-content: center;
-`;
-
-const Container = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-item: center;
-width: 90%;
-max-width: 60rem;
-`;
-
-function Header () {
-  return (
-    <WrapperHeader>
-      <Container>
-        <div className="header-logo">
-          <img src="" alt="logo"/>
-          <h1>Space Traverlers&apos; Hub</h1>
-        </div>
-        <nav>
-          <ul>
-            <li>Rockets</li>
-            <li>Mission</li>
-            <li>My Profile</li>
-          </ul>
-        </nav>
-
-      </Container>
-    </WrapperHeader>
-  );
-};
+const Header = () => (
+  <header>
+    <div>
+      <img src="planet.png" alt="logo" />
+      <h1>Space Traverlers&apos; Hub</h1>
+    </div>
+    <nav>
+      <ul>
+        <li>Rockets</li>
+        <li>Mission</li>
+        <li>My Profile</li>
+      </ul>
+    </nav>
+  </header>
+);
 export default Header;
