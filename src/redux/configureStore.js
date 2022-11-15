22fd3158e7a38e0efa44e365f1ee7ae359959a4d
Shedrack Sunday <Hannania002@gmail.com>
2022-11-15 17:22:10 +0100

    Add reducers for Rockets file in configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import reducersRockets from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
+    rockets: reducersRockets,
   },
+  middleware: [logger]
 });
 
 export default store;
