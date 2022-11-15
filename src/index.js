943566be384bce3c857c003eefee7f3ddcd7a184
Shedrack Sunday <Hannania002@gmail.com>
2022-11-15 17:02:20 +0100

    Add <Provider> to index.js and store  redux state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from './redux/configureStore';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+  document.getElementById('root'),
 );
