f1ea0455ba5e54105c1c928d6df96fd827a5ac2a
Shedrack Sunday <Hannania002@gmail.com>
2022-11-15 17:33:09 +0100

    Add createSlice reducers to redux/Rockets file

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const rocketsSlice = createSlice({
+    name: 'rockets',
+    initialStore: {
+        counterRocket: 0,
+    },
+    reducers: {
+        incrementRocket: (state) => ({
+            ...state,
+            counterRocket: state.counterRocket + 1,
+        }),
+    },
+});
+
+export const { incrementRocket } = rocketsSlice.actions;
+export const selectCounterRocket = (state) => state.rockets.counterRocket;
+
+export default rocketsSlice.reducers;
