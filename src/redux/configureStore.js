0bdeb20b4683dc321af46353d023aab0fd9d58de
Shedrack Sunday <Hannania002@gmail.com>
2022-11-16 12:27:43 +0100

    Add Thunk to configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducersRockets from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
     rockets: reducersRockets,
   },
-  middleware: [logger],
+  middleware: [thunk, logger],
 });
 
 export default store;
