9877a5eff90c6d02c82620c09a0e38e5f718fedf
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-11-16 13:55:46 +0330

    Change to createRoot to be compatible with react 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import store from './redux/configureStore';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
