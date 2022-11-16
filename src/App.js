9b9e3ac9f3722d832e0e28179ab8583a5fc38da9
Shedrack Sunday <Hannania002@gmail.com>
2022-11-16 12:27:23 +0100

    Create disptch for API Data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { fetchApiRockets } from './redux/rockets/rockets';
 import Header from './components/Header';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
@@ -7,6 +9,11 @@ import Profile from './pages/Profile';
 import './App.css';
 
 function App() {
+  const API_ROCKETS = 'https://api.spacexdata.com/v3/rockets';
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchApiRockets(API_ROCKETS));
+  }, []);
   return (
     <Router className="App">
       <Header />
